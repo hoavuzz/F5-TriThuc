027b456e587e26a3596e7745fde902ce1ca7a2e4
hoavuzz <anhhoa12aa@gmail.com>
2025-07-23 23:54:43 +0700

    abc

---
+++ site/view/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Trang Chủ - E-Learning</title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+ <link rel="stylesheet" href="/F5-TRITHUC/public/css/site.css">
+
+</head>
+<body>
+  <header>
+    
+    <div class="logo">F5</div>
+    <nav>
+      <a href="#">Khóa học</a>
+      <a href="#">Tài liệu</a>
+      <a href="#">Về chúng tôi</a>
+      <a href="#">Đăng ký</a>
+      <a href="#">Đăng nhập</a>
+    </nav>
+  </header>
