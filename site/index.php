9fb446760c55744e1e7033f89b81d71cdc940e85
ThanhF <thanhnvtps40502@gmail.com>
2025-07-25 15:36:55 +0700

    Thêm file upload.php trong view

--- site/index.php
+++ site/index.php
@@ -4,25 +4,25 @@ session_start();
 // echo "đây là trang index nè"
 //điều hướng đến các controller;
 $ctrl = isset($_GET['mod']) ? ($_GET['mod']) : 'page';
-require_once("../site/views/header.php");
-switch ($ctrl) {
-    case 'page':
-        include_once "../site/controllers/PageController.php";
+require_once("../site/view/header.php");
+// switch ($ctrl) {
+//     case 'page':
+//         include_once "../site/controllers/PageController.php";
 
-        break;
-    case 'user':
+//         break;
+//     case 'user':
 
-        include_once "../site/controllers/UserController.php";
-        break;
-    case 'product':
-        include_once "../site/controllers/ProductController.php";
-        break;
-    case 'cart';
-        include_once "../site/controllers/cartcontroller.php";
-        break;
-    default:
+//         include_once "../site/controllers/UserController.php";
+//         break;
+//     case 'product':
+//         include_once "../site/controllers/ProductController.php";
+//         break;
+//     case 'cart';
+//         include_once "../site/controllers/cartcontroller.php";
+//         break;
+//     default:
 
-        break;
+//         break;
 
-}
-include_once("../site/views/footer.php");
+// }
+include_once("../site/view/footer.php");
